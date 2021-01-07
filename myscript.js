b78499b05e41e999eb1c48b4c7121a77be34859e
Afrah79 <afrahmubarakalazmi@gmail.com>
2021-01-07 22:12:29 +0300

    done

--- myscript.js
+++ myscript.js
@@ -4,11 +4,51 @@ let board = [
     ['20', '21', '22']
 ]
 
+let turn = "o";
+
+function press(x,y) {
+    if (turn == "o" ){
+        turn = "x";
+    }
+    else{
+        turn = "o";
+    }
+
+    board[x][y] = turn;
+    let cell = document.getElementById(`${x}${y}`)
+    cell.innerText = turn;
+  
 
 
-function press(cellNO) {
+    console.log(x,y)
 
-    // Do stuff
-    console.log(cellNO)
-    
+    if (board[0][0]== turn && board[0][1] == turn && board[0][2]== turn){
+        document.getElementById("status").innerText= `winner is ${turn}`
+    }
+    if (board[1][0]== turn && board[1][1] == turn && board[1][2]== turn){
+        document.getElementById("status").innerText= `winner is ${turn}`
+    }
+    if (board[2][0]== turn && board[2][1] == turn && board[2][2]== turn){
+        document.getElementById("status").innerText= `winner is ${turn}`
+    }
+    if (board[0][0]== turn && board[1][1] == turn && board[2][2]== turn){
+        document.getElementById("status").innerText= `winner is ${turn}`
+    }
+    if (board[0][2]== turn && board[1][1] == turn && board[2][0]== turn){
+        document.getElementById("status").innerText= `winner is ${turn}`
+    }
+    if (board[0][0]== turn && board[1][0] == turn && board[2][0]== turn){
+        document.getElementById("status").innerText= `winner is ${turn}`
+    } 
+    if (board[0][1]== turn && board[1][1] == turn && board[2][1]== turn){
+        document.getElementById("status").innerText= `winner is ${turn}`
+    }
+    if (board[0][2]== turn && board[1][2] == turn && board[2][2]== turn){
+        document.getElementById("status").innerText= `winner is ${turn}`
+    }
 }
+function reset(x,y){
+    turn = "";
+
+}
+ 
